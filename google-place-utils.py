3f396a77bfffe5479b858a0d5a60cf31e3b1f1cf
jordankckoh <75975320+jordankckoh@users.noreply.github.com>
2025-03-02 16:19:03 +0800

    Update google-place-utils.py

--- google-place-utils.py
+++ google-place-utils.py
@@ -55,7 +55,7 @@ def get_location_details_from_place_id(place_id, api_key):
     Returns:
         dict: Location details
     """
-    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,rating,reviews,formatted_address&key={api_key}"
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,rating,reviews,formatted_address&key={AIzaSyATw_I-Qfk2jF3rW-qhr5Er9KQ67PjocGo}"
     
     try:
         response = requests.get(url)
